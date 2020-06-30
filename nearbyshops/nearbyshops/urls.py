39657074f0c5e3ef179fcbfaadccfe8d7ac1a807
fc-anjos <felipe.anjos@usp.br>
2020-06-30 18:10:43 -0300

    update requirements

--- nearbyshops/nearbyshops/urls.py
+++ nearbyshops/nearbyshops/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from shops import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.points_view),
+    path('admin', admin.site.urls),
+    path('points', views.points_view),
 ]
