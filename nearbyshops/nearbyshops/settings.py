16da22ff88e97db02d0dabb7090e0464e3e04da6
fc-anjos <felipe.anjos@usp.br>
2020-07-01 11:33:46 -0300

    set up devserver

--- nearbyshops/nearbyshops/settings.py
+++ nearbyshops/nearbyshops/settings.py
@@ -129,8 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
+STATIC_URL = 'http://127.0.0.1:8080/'
 STATICFILES_DIRS = ['dist']
 
 LEAFLET_CONFIG = {
