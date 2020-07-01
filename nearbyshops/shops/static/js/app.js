4d52eef1b6abf18877fb7a5720c1b26fecd6d90a
fc-anjos <felipe.anjos@usp.br>
2020-07-01 12:51:19 -0300

    add google maps basemap

--- nearbyshops/shops/static/js/app.js
+++ nearbyshops/shops/static/js/app.js
@@ -1,2 +1,9 @@
-import './name';
-import './counter';
+window.initMap = () => {
+
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -23.5489, lng: -46.6388},
+        zoom: 12,
+    });
+
+}
+
