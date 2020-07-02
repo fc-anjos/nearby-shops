85a480d4a7e7bac98cd8fcfe5b8919f2d97ef08c
fc-anjos <felipe.anjos@usp.br>
2020-07-02 08:49:49 -0300

    hot module replacement with js

--- nearbyshops/shops/static/js/app.js
+++ nearbyshops/shops/static/js/app.js
@@ -1,9 +1,3 @@
-window.initMap = () => {
-
-    const map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -23.5489, lng: -46.6388},
-        zoom: 12,
-    });
-
-}
+import './counter';
+import './name';
 
