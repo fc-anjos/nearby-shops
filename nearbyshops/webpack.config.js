ffdafff1eb386ccc9606b43a6e01e5cabe598b09
fc-anjos <felipe.anjos@usp.br>
2020-07-02 09:24:21 -0300

    add babel transpiler

--- nearbyshops/webpack.config.js
+++ nearbyshops/webpack.config.js
@@ -2,13 +2,23 @@ module.exports = {
   mode: 'development',
   entry: './shops/static/js/app.js',
   output: {
-    publicPath: 'http://127.0.0.1:8080/'
+    publicPath: 'http://127.0.0.1:8080/',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
